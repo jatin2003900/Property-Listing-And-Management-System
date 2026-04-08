2f3ef6a8b846f0d7cde9e58c157cc3672e4b1f33
Jatin <jatinkrsingh.123@gmail.com>
2026-04-08 18:08:07 +0530

    converted mern-estate from submodule to normal folder

---
+++ mern-estate/client/src/pages/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Real Estate</h1>
+      <p className='mb-4 text-slate-700'>Welcome to Real Estate – your trusted digital partner in real estate.
+
+At This Real Estate, we’re redefining the way people buy, sell, and list properties. Built with cutting-edge technology and a customer-first approach, our platform is designed to simplify the real estate experience for individuals, families, and investors alike.</p>
+      <p className='mb-4 text-slate-700'>
+      Our Mission
+To empower customers with a seamless, secure, and transparent platform to manage all their real estate needs — from listing properties for sale to finding their dream home.
+
+What We Offer
+🏡 List Your Property with Ease
+Whether you’re an individual owner or a real estate agent, Real Estate offers a simple and intuitive interface to list your residential or commercial properties. Upload images, set prices, add descriptions — all in a few clicks.
+
+🛒 Buy Properties with Confidence
+Discover a wide range of verified property listings across various locations. Filter by price, type, or location, and explore properties with detailed descriptions, photos, and neighborhood insights.
+
+🔁 Sell Faster with Smarter Tools
+Reach thousands of potential buyers by showcasing your property on a reliable and feature-rich platform. Our smart recommendations and optimized listing features help you connect with the right buyers.
+
+🔍 Real-Time Search & Location-Based Results
+Utilize our powerful search engine with real-time filtering and location detection to find the most relevant listings based on your needs and budget.
+      </p>
+      <p className='mb-4 text-slate-700'></p>
+    </div>
+  )
+}
